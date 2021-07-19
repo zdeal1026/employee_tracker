316eeb9ba49f27e61769324a06e21a9edd52725e
Zdeal <jedimastershake@gmail.com>
2021-07-18 20:24:43 -0400

    database connection

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const inquirer = require("inquirer");
 const mysql= require("mysql");
 const console = require("console.table");
 
-
+//gets connection to database and portion
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3434;
+    user: 'root',
+    password: '',
+    database: 'tracker_DB',
+});
 
