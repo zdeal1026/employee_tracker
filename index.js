1d99d0f6c800e397490098d217782a00e46755a4
Zdeal <jedimastershake@gmail.com>
2021-07-18 20:33:29 -0400

    working index

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ const connection = mysql.createConnection({
     database: 'tracker_DB',
 });
 
+//viewing employees
+const employee = () => {
+    console.log('Showing Employee Data');
+    connection.query(
+        `SELECT employee.first_name , employeee.last_name, title, salary, managers.first_name AS manager_name
+        FROM employee
+        LEFT JOIN employee managers ON employee.manager_id = managers.id
+        INNER JOIN role ON employee.role_id = role.id`,
+
+        (err,results) => {
+            if (err) throw err;
+            console.table(results);
+            init();
+        }
+    );
+};
+
